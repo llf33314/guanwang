59b290b4b8ab45f91e24e7ec0f773e982403e792
zhouweiquan <473108784@qq.com>
2017-08-07 17:29:42 +0800

    官网

--- WebContent/html/homeland/js/_index.js
+++ WebContent/html/homeland/js/_index.js
@@ -39,7 +39,7 @@ $(function(){
 		}else{
 			$('#paytip').hide();
 		}
-		a.source = "电脑-样子";
+		a.source = "电脑-物业";
 		var v = WSFUNCTION.doPostReturnFull("/webOrder/html/add", a);
 		if(v != null && v.status == 1){
 			/*if(v.type == 0){*/
